01a1360dceae9c9363958ceed3ef3cb1e73c99d6
mact200590 <mact200590@gmail.com>
2020-05-17 16:12:24 -0400

    add-verification-token

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ColumnChart from "../containers/ColumnChart";
 import { Header } from "../components/Header";
+import { verificationTokeStorage } from '../utils'
+import { useHistory } from "react-router-dom"
 
 const Dashboard = () => {
-
+  const { push } = useHistory();
+  const redirect = verificationTokeStorage(window.localStorage.getItem("isLogged"));
   return (
     <>
+     {redirect && (push("/login"))}
       <Header />
       <ColumnChart />
     </>
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,9 @@
 export const DateFormat = 'YYYY-MM-DD';
+
+export const verificationTokeStorage = (valueStorage: string | null) => {
+    if (valueStorage === null) {
+        return true;
+    } else {
+        return false;
+    }
+}

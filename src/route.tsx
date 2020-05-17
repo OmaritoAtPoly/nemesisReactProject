b4124f53863e28eebf472c92437886499267e1ad
elyanio <48162982+elyanio@users.noreply.github.com>
2020-05-17 07:37:17 -0500

    rename to Dashboard

--- src/route.tsx
+++ src/route.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route } from "react-router-dom";
 import  Login  from './pages/Login';
-import Dashboard from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 export const Router = () => {
     return (
